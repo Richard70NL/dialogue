1f11ac9e48b941849887bdf1a38f86c501fa7ff3
Richard70NL <richard@richard70.nl>
2019-04-28 23:32:27 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -137,9 +137,7 @@ fn run() -> Result<(), DialogueError> {
 
 fn start_server(verbose: &Verbose, address: &str, port: &str, dburl: &str) {
     verbose.println("Initializing the server.");
-
     let mut server = Server::new();
-
     server.set_binding_address(IpAddr::from_str(address).unwrap()); // FIXME unwrap
     server.set_binding_port(port.parse::<u16>().unwrap()); // FIXME unwrap
     server.set_database_url(String::from(dburl));
