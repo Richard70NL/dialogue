908d6b30b5c615b6bf100c094e8738c45ba43355
Richard70NL <richard@richard70.nl>
2019-04-30 16:26:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# - Set address to 0.0.0.0 to bind to all IP addresses on the host.
-# - Set address to 127.0.0.1 to bind to the loopback address which limits
-#   access to the localhost only (best option during development).
+# - Set address to 0.0.0.0 (:: for IPv6) to bind to all IP addresses on the
+#   host.
+# - Set address to 127.0.0.1 (::1 for IPv6) to bind to the loopback address
+#   which limits access to the localhost only (best option during development).
 # - Or set address to specific IP.
-cargo run -- start --verbose --address=::
+cargo run -- start --verbose --address=127.0.0.1
