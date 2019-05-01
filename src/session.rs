46f371da1d9cc3d3814155f76c7338b5800201f0
Richard70NL <richard@richard70.nl>
2019-05-02 01:14:05 +0200

    Update session.rs

--- src/session.rs
+++ src/session.rs
@@ -1,6 +1,7 @@
 /************************************************************************************************/
 
 use crate::log::LogMessage;
+use crate::log::LogMessageType::*;
 use crate::text::s;
 use crate::text::Text::*;
 use std::io::BufRead;
@@ -35,11 +36,12 @@ impl Session {
         let mut reader = BufReader::new(&self.stream);
         let mut writer = BufWriter::new(&self.stream);
         let mut line = String::new();
+        let peer_addr = self.stream.peer_addr().unwrap();
 
         if self.posting_allowed {
             write_response_and_log(
                 &mut writer,
-                self.stream.peer_addr().unwrap(),
+                peer_addr,
                 200,
                 s(ResponseServiceAvailPostAllowed),
                 s(LogConnectionAccepted),
@@ -47,19 +49,41 @@ impl Session {
         } else {
             write_response_and_log(
                 &mut writer,
-                self.stream.peer_addr().unwrap(),
+                peer_addr,
                 201,
                 s(ResponseServiceAvailPostProhibited),
                 s(LogConnectionAccepted),
             );
         }
 
-        loop {
+        'main_loop: loop {
             line.clear();
             match reader.read_line(&mut line) {
                 Ok(_len) => {
-                    let command_line = line.trim();
-                    writeln(&mut writer, &format!("echo: {}", command_line));
+                    let command = parse_command_line(line.trim());
+                    writeln(&mut writer, &format!("echo: {:?}", command));
+
+                    if command.len() > 0 {
+                        match command[0].to_lowercase().as_str() {
+                            "quit" | "exit" | "logout" => {
+                                write_response_and_log(
+                                    &mut writer,
+                                    peer_addr,
+                                    205,
+                                    "Connection closing.", // FIXME use text module
+                                    &format!("received: {:?}", command), // FIXME use text module
+                                );
+                                break 'main_loop;
+                            }
+                            &_ => write_response_and_error(
+                                &mut writer,
+                                peer_addr,
+                                500,
+                                "Unknown command.", // FIXME use text module
+                                &format!("received: {:?}", command), // FIXME use text module
+                            ),
+                        }
+                    }
                 }
                 Err(e) => eprintln!("{}", e), // FIXME write propper error response
             }
@@ -86,7 +110,25 @@ fn write_response_and_log(
 ) {
     write_response(writer, response_code, message);
 
-    LogMessage::new(format!("{} [{}]", log_message, message))
+    LogMessage::new(format!("{}; response: {}", log_message, message))
+        .set_response_code(response_code)
+        .set_client_addr(peer_addr)
+        .show();
+}
+
+/************************************************************************************************/
+
+fn write_response_and_error(
+    writer: &mut BufWriter<&TcpStream>,
+    peer_addr: SocketAddr,
+    response_code: u16,
+    message: &str,
+    log_message: &str,
+) {
+    write_response(writer, response_code, message);
+
+    LogMessage::new(format!("{}; response: {}", log_message, message))
+        .set_type(Error)
         .set_response_code(response_code)
         .set_client_addr(peer_addr)
         .show();
@@ -103,6 +145,16 @@ fn writeln(writer: &mut BufWriter<&TcpStream>, line: &str) {
 fn write(writer: &mut BufWriter<&TcpStream>, s: &str) {
     writer.write(s.as_bytes()).unwrap();
     writer.flush().unwrap();
+}
+
+/************************************************************************************************/
+
+fn parse_command_line(command_line: &str) -> Vec<String> {
+    let command_line_string = String::from(command_line);
+    let iter = command_line_string.split_ascii_whitespace();
+    let command: Vec<String> = iter.map(|s| String::from(s)).collect();
+
+    command
 }
 
 /************************************************************************************************/
